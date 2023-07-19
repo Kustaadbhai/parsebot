8c452c65238fc5753fb8dad1b3caefb206eaeb22
Ahmad Rehan <ahmedrehan10@gmail.com>
2023-07-19 17:46:02 +0500

    make it better

--- index.js
+++ index.js
@@ -103,6 +103,7 @@ app.post('/chatbot', async (req, res) => {
 });
 
 // Start the server
-app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
+let port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log('Example app listening on port ' + port + '!');
 });
